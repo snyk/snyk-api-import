228332ebc5cf0cfac1a949583ab1052fe9c064ed
ghe <lili@snyk.io>
2022-03-03 11:12:15 +0000

    fix: exit code 1 if orgs:data errors

--- test/system/orgs:data/errors.test.ts
+++ test/system/orgs:data/errors.test.ts
@@ -17,4 +17,25 @@ describe('General `snyk-api-import orgs:data <...>`', () => {
       done();
     });
   });
+  it('Shows error when token is invalid', (done) => {
+    const groupId = 'hello';
+    exec(
+      `node ${main} orgs:data --source=github --groupId=${groupId}`,
+      {
+        env: {
+          PATH: process.env.PATH,
+          GITHUB_TOKEN: 'invalid',
+          SNYK_LOG_PATH: __dirname,
+        },
+      },
+      (err, stdout, stderr) => {
+        expect(err!.message).toMatch('ERROR: Bad credentials');
+        expect(stdout).toEqual('');
+        expect(stderr).toMatch('ERROR: Bad credentials');
+      },
+    ).on('exit', (code) => {
+      expect(code).toEqual(1);
+      done();
+    });
+  });
 });
