c343971baf06c716b92dd1a679830c046ce340c9
Mathilde <mathilde.riou@snyk.io>
2022-10-13 16:54:29 +0100

    feat: implement compare branch for project

---
+++ src/lib/project/compare-branches.ts
@@ -0,0 +1,29 @@
+import debug = require("debug");
+import { requestsManager } from "snyk-request-manager";
+import { updateProject } from "../api/project";
+
+export async function compareAndUpdateBranches(
+    requestManager: requestsManager,
+    snykDefaultBranch: string,
+    githubBranch: string,
+    projectId: string,
+    orgId: string,
+  ): Promise<{projectUpdated:boolean}> {
+
+    let projectUpdated = false
+    try {
+      
+      if (snykDefaultBranch != githubBranch) {
+        debug(`default branch update needed for project ${projectId}\n`);
+        
+          await updateProject(requestManager, orgId, projectId, {branch: githubBranch});
+          projectUpdated = true
+      }
+
+    return { projectUpdated: projectUpdated } 
+  } catch (e) {
+    throw new Error(
+      `Failed to update project ${projectId}. ERROR: ${e.message}`,
+    );
+  }
+}
