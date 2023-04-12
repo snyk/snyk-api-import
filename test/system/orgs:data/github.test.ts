8be4ff483fc38712eabb4e4c2f8438d080e313d4
ghe <lili@snyk.io>
2023-04-12 19:19:19 +0100

    chore: drop usage of gh_token

--- test/system/orgs:data/github.test.ts
+++ test/system/orgs:data/github.test.ts
@@ -15,7 +15,7 @@ describe('General `snyk-api-import orgs:data <...>`', () => {
       {
         env: {
           PATH: process.env.PATH,
-          GITHUB_TOKEN: process.env.GH_TOKEN,
+          GITHUB_TOKEN: process.env.GITHUB_TOKEN,
           SNYK_LOG_PATH: __dirname,
         },
       },
@@ -26,7 +26,7 @@ describe('General `snyk-api-import orgs:data <...>`', () => {
         expect(stderr).toEqual('');
         expect(err).toBeNull();
         expect(stdout).toMatch(
-          'Found 4 organization(s). Written the data to file: group-hello-github-com-orgs.json',
+          'Found 2 organization(s). Written the data to file: group-hello-github-com-orgs.json',
         );
         deleteFiles([
           path.resolve(__dirname, `group-${groupId}-github-com-orgs.json`),
