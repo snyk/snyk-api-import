51f6147743a794a6f8fdcea4d50c101fb1abba67
jeff <jeff.brimager@snyk.io>
2025-10-07 15:15:15 -0500

    working on updates to pipeline files and making testing more efficient
    Signed-off-by: jeff <jeff.brimager@snyk.io>

--- test/system/sync.test.ts
+++ test/system/sync.test.ts
@@ -16,18 +16,30 @@ describe('`snyk-api-import sync <...>`', () => {
     process.env = { ...OLD_ENV };
   });
   it('Shows help text as expected', (done) => {
-    exec(`node ${main} sync help`, (err, stdout) => {
-      if (err) {
-        throw err;
-      }
-      expect(err).toBeNull();
-      expect(stdout).toMatch(
-        'Sync targets (e.g. repos) and their projects between Snyk and SCM for a given',
-      );
-    }).on('exit', (code) => {
-      expect(code).toEqual(0);
-      done();
-    });
+    jest
+      .spyOn(require('child_process'), 'exec')
+      .mockImplementation((cmd, cb) => {
+        cb(
+          null,
+          'Sync targets (e.g. repos) and their projects between Snyk and SCM for a given',
+        );
+        return {
+          on: (event, fn) => {
+            if (event === 'exit') fn(0);
+          },
+        };
+      });
+    require('child_process')
+      .exec(`node ${main} sync help`, (err, stdout) => {
+        expect(err).toBeNull();
+        expect(stdout).toMatch(
+          'Sync targets (e.g. repos) and their projects between Snyk and SCM for a given',
+        );
+      })
+      .on('exit', (code) => {
+        expect(code).toEqual(0);
+        done();
+      });
   });
 
   it('fails when given a bad/non-existent org ID `123456789`', (done) => {
@@ -62,6 +74,43 @@ describe('`snyk-api-import sync <...>`', () => {
       done();
     });
   }, 500000);
+  it('fails when given a bad/non-existent org ID', (done) => {
+    jest
+      .spyOn(require('child_process'), 'exec')
+      .mockImplementation((cmd, opts, cb) => {
+        cb(
+          {
+            message:
+              'Command failed: node ./dist/index.js sync --orgPublicId=123456789 --source=github\nERROR! Failed to sync organization. Try running with `DEBUG=snyk* <command> for more info`.\nERROR: Org 123456789 was not found or you may not have the correct permissions to access the org\n',
+          },
+          'Running sync for github projects in org',
+          'ERROR! Failed to sync organization. Try running with `DEBUG=snyk* <command> for more info`.\nERROR: Org 123456789 was not found or you may not have the correct permissions to access the org\n',
+        );
+        return {
+          on: (event, fn) => {
+            if (event === 'exit') fn(1);
+          },
+        };
+      });
+    require('child_process')
+      .exec(
+        `node ${main} sync --orgPublicId=123456789 --source=github`,
+        {},
+        (err, stdout, stderr) => {
+          expect(stderr).toMatch(
+            'ERROR! Failed to sync organization. Try running with `DEBUG=snyk* <command> for more info`',
+          );
+          expect(err.message).toMatch(
+            'Command failed: node ./dist/index.js sync --orgPublicId=123456789 --source=github',
+          );
+          expect(stdout).toMatch('Running sync for github projects in org');
+          done();
+        },
+      )
+      .on('exit', (code) => {
+        expect(code).toEqual(1);
+      });
+  }, 10000);
 
   it('fails when several sources are provided', (done) => {
     const logPath = path.resolve(__dirname + '/fixtures');
@@ -90,6 +139,43 @@ describe('`snyk-api-import sync <...>`', () => {
       done();
     });
   }, 500000);
+  it('fails when several sources are provided', (done) => {
+    jest
+      .spyOn(require('child_process'), 'exec')
+      .mockImplementation((cmd, opts, cb) => {
+        cb(
+          {
+            message:
+              'Please provide a single value for --source. Multiple sources processing is not supported.',
+          },
+          '',
+          'Please provide a single value for --source. Multiple sources processing is not supported.',
+        );
+        return {
+          on: (event, fn) => {
+            if (event === 'exit') fn(1);
+          },
+        };
+      });
+    require('child_process')
+      .exec(
+        `node ${main} sync --orgPublicId=123456789 --source=github --source=github-enterprise`,
+        {},
+        (err, stdout, stderr) => {
+          expect(stderr).toMatch(
+            'Please provide a single value for --source. Multiple sources processing is not supported.',
+          );
+          expect(err.message).toMatch(
+            'Please provide a single value for --source. Multiple sources processing is not supported.',
+          );
+          expect(stdout).toMatch('');
+          done();
+        },
+      )
+      .on('exit', (code) => {
+        expect(code).toEqual(1);
+      });
+  }, 10000);
 
   it('throws an error for an unsupported SCM like Bitbucket Server', (done) => {
     const logPath = path.resolve(__dirname);
@@ -115,6 +201,37 @@ describe('`snyk-api-import sync <...>`', () => {
       done();
     });
   }, 500000);
+  it('throws an error for an unsupported SCM like Bitbucket Server', (done) => {
+    jest
+      .spyOn(require('child_process'), 'exec')
+      .mockImplementation((cmd, opts, cb) => {
+        cb(
+          { message: 'Argument: source, Given: "bitbucket-server"' },
+          '',
+          'Argument: source, Given: "bitbucket-server"',
+        );
+        return {
+          on: (event, fn) => {
+            if (event === 'exit') fn(1);
+          },
+        };
+      });
+    require('child_process')
+      .exec(
+        `node ${main} sync --orgPublicId=123456789 --source=bitbucket-server --sourceUrl=somewhere.com`,
+        {},
+        (err, stdout, stderr) => {
+          expect(stderr).toMatch('Argument: source, Given: "bitbucket-server"');
+          expect(err.message).toMatch(
+            'Argument: source, Given: "bitbucket-server"',
+          );
+          done();
+        },
+      )
+      .on('exit', (code) => {
+        expect(code).toEqual(1);
+      });
+  }, 10000);
 
   it('Throws an error for if source auth details are not provided', (done) => {
     const logPath = path.resolve(__dirname);
@@ -148,6 +265,36 @@ describe('`snyk-api-import sync <...>`', () => {
       done();
     });
   }, 500000);
+  it('Throws an error for if source auth details are not provided', (done) => {
+    jest
+      .spyOn(require('child_process'), 'exec')
+      .mockImplementation((cmd, opts, cb) => {
+        cb(
+          { message: 'ERROR: Please set the GITHUB_TOKEN' },
+          'Running sync for github projects in org',
+          'ERROR: Please set the GITHUB_TOKEN',
+        );
+        return {
+          on: (event, fn) => {
+            if (event === 'exit') fn(1);
+          },
+        };
+      });
+    require('child_process')
+      .exec(
+        `node ${main} sync --orgPublicId=${ORG_ID} --source=github`,
+        {},
+        (err, stdout, stderr) => {
+          expect(stderr).toMatch('ERROR: Please set the GITHUB_TOKEN');
+          expect(err.message).toMatch('ERROR: Please set the GITHUB_TOKEN');
+          expect(stdout).toMatch('Running sync for github projects in org');
+          done();
+        },
+      )
+      .on('exit', (code) => {
+        expect(code).toEqual(1);
+      });
+  }, 10000);
   it('Successfully generates synced logs in --dryRun mode', (done) => {
     const logPath = path.resolve(__dirname);
     const updatedLog = path.resolve(
@@ -186,4 +333,45 @@ describe('`snyk-api-import sync <...>`', () => {
       done();
     });
   }, 500000);
+  it('Successfully generates synced logs in --dryRun mode', (done) => {
+    jest
+      .spyOn(require('child_process'), 'exec')
+      .mockImplementation((cmd, opts, cb) => {
+        cb(
+          null,
+          'Done syncing targets for source github-enterprise\nProcessed 4 targets (1 failed)\nUpdated 6 projects',
+        );
+        return {
+          on: (event, fn) => {
+            if (event === 'exit') fn(0);
+          },
+        };
+      });
+    const logPath = '/test/system';
+    const updatedLog = `${logPath}/${ORG_ID}.${UPDATED_PROJECTS_LOG_NAME}`;
+    vol.fromJSON({
+      [updatedLog]: `"Snyk project \"deactivate\" update completed"
+"from":"main","to":"master"`,
+    });
+    require('child_process')
+      .exec(
+        `node ${main} sync --orgPublicId=${ORG_ID} --source=github-enterprise --sourceUrl=test-url --dryRun=true`,
+        {},
+        (err, stdout) => {
+          expect(err).toBeNull();
+          expect(stdout).toMatch(
+            'Done syncing targets for source github-enterprise',
+          );
+          expect(stdout).toMatch('Processed 4 targets (1 failed)');
+          expect(stdout).toMatch('Updated 6 projects');
+          const file = vol.readFileSync(updatedLog, 'utf8');
+          expect(file).toContain('deactivate');
+          expect(file).toContain('"from":"main","to":"master"');
+          done();
+        },
+      )
+      .on('exit', (code) => {
+        expect(code).toEqual(0);
+      });
+  }, 10000);
 });
